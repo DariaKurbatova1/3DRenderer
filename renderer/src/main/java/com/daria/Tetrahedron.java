46f56d11682dd53736841f16c873ff710195172d
DariaKurbatova02 <daria.kurbatova@dawsoncollege.qc.ca>
2024-10-22 13:11:48 -0400

    add Tetrahedron class

---
+++ renderer/src/main/java/com/daria/Tetrahedron.java
@@ -0,0 +1,15 @@
+package com.daria;
+import com.daria.Triangle;
+
+public class Tetrahedron {
+    Triangle t1;
+    Triangle t2;
+    Triangle t3;
+    Triangle t4;
+    Tetrahedron(Triangle t1, Triangle t2, Triangle t3, Triangle t4){
+        this.t1 = t1;
+        this.t2 = t2;
+        this.t3= t3;
+        this.t4 = t4;
+    }
+}
--- renderer/src/main/java/com/daria/Vertex.java
+++ renderer/src/main/java/com/daria/Vertex.java
@@ -1,8 +1,11 @@
 package com.daria;
 
 public class Vertex {
+    //movement in left-right direction
     double x;
+    //movement in up-down direction
     double y;
+    //depth(movement towards/away user)
     double z;
 
     Vertex(double x, double y, double z){
