72a1aaf8c3b857bbe607c603a05eb8c5778404dc
DariaKurbatova02 <daria.kurbatova@dawsoncollege.qc.ca>
2024-10-22 13:03:58 -0400

    add vertex class

---
+++ renderer/src/main/java/com/daria/Vertex.java
@@ -0,0 +1,13 @@
+package com.daria;
+
+public class Vertex {
+    double x;
+    double y;
+    double z;
+
+    Vertex(double x, double y, double z){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
