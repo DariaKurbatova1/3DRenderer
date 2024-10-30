2ef6ec8b980a24b14b6ba243312b33b55b065957
DariaKurbatova02 <daria.kurbatova@dawsoncollege.qc.ca>
2024-10-30 12:36:42 -0400

    add shape factory class

---
+++ renderer/src/main/java/com/daria/ShapeFactory.java
@@ -0,0 +1,12 @@
+package com.daria;
+
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShapeFactory {
+    public Tetrahedron tetrahedron;
+    public Cube cube;
+
+
+}
